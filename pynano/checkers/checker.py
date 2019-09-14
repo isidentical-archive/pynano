7294d670657f1aa32d7fbf5c987e0f77250b79e0
isidentical <batuhanosmantaskaya@gmail.com>
2019-09-14 09:56:17 +0300

    increase typing

--- pynano/checkers/checker.py
+++ pynano/checkers/checker.py
@@ -1,5 +1,5 @@
 import ast
-from typing import Union
+from typing import Type, Union
 
 ACTIVE_SIGN = "ACTIVE"
 CHECKERS = []
@@ -8,14 +8,14 @@ CHECKERS = []
 class NanoSyntaxError(SyntaxError):
     # https://github.com/python/cpython/blob/5b9ff7a0dcb16d6f5c3cd4f1f52e0ca6a4bde586/Objects/exceptions.c#L1355
 
-    def __init__(self, msg, node):
+    def __init__(self, msg: str, node: ast.AST) -> None:
         super().__init__(msg, (None, node.lineno, node.col_offset, None))
 
 
 class SyntaxChecker(ast.NodeVisitor):
-    def __init_subclass__(cls):
+    def __init_subclass__(cls: Type[SyntaxChecker]) -> None:
         if getattr(cls, ACTIVE_SIGN, None):
             CHECKERS.append(cls)
 
-    def check(self, tree, *, strict=True) -> bool:
+    def check(self, tree: ast.AST, *, strict: bool = True) -> bool:
         return self.visit(tree) or True
--- pynano/pyn.py
+++ pynano/pyn.py
@@ -1,16 +1,23 @@
 import ast
 import os
 import tokenize
-from typing import Optional, Sequence
+from typing import List, Optional, Sequence, Type
 
 from pynano.checkers import CHECKERS, SyntaxChecker
 
+Checkers = Sequence[Type[SyntaxChecker]]
+
 
 class PyNano:
-    def __init__(self, checkers: Optional[Sequence[SyntaxChecker]]) -> None:
-        self.checkers = checkers or CHECKERS
+    def __init__(self, checkers: Optional[Checkers]) -> None:
+        self.checkers: List[SyntaxChecker] = []
+        self._pre_init(checkers or CHECKERS)
 
-    def compile(self, file_path: os.PathLike):
+    def _pre_init(self, checkers: Checkers, **options) -> None:
+        for checker in checkers:
+            self.checkers.append(checker())
+
+    def compile(self, file_path: os.PathLike) -> None:
         file_path = os.fspath(file_path)
 
         with tokenize.open(file_path) as file:
