29ece35782cacfb3cfcf35bf9b50e6a11cf44d34
Batuhan Taskaya <isidentical@gmail.com>
2022-01-01 15:44:50 +0300

    Simplify the setup instructions

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="pynano",
-    version="0.1.0a0",
-    packages=find_packages(),
-    url="https://github.com/pynano/pynano",
-    author="btaskaya",
-    description="PyNano compiler",
-)
+setup(name="pynano", version="0.1.0a0")
